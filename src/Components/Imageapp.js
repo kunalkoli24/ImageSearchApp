c67c6d3554aa3dd5065518fc0fe86c7d6959894b
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-02-04 20:20:46 +0530

    img alt

--- src/Components/Imageapp.js
+++ src/Components/Imageapp.js
@@ -1,45 +1,46 @@
-import React, { useState } from 'react'
- 
+import React, { useState } from 'react';
+
 const Imageapp = () => {
-  const [search, setSearch] = useState();
+  const [search, setSearch] = useState('');
   const [data, setData] = useState([]);
 
-  let API_KEY = "uGj5mxmIILPu8Aci9nVMCYDvbU_NEM177EFymc_oZMo"
-  const handleSearch = (event) =>{
-    setSearch(event.target.value)
+  let API_KEY = "uGj5mxmIILPu8Aci9nVMCYDvbU_NEM177EFymc_oZMo";
+  
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
 
-  }
-  const getData = () =>{
-    myFun(search)
-  }
-  const myFun = async(searchVal) =>{
-    const get = await fetch(`https:/api.unsplash.com/search/photos?page=1&query=${searchVal}&client_id=${API_KEY}`);
-    const jsonData  = await get.json()
+  const getData = () => {
+    myFun(search);
+  };
+
+  const myFun = async (searchVal) => {
+    const get = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchVal}&client_id=${API_KEY}`);
+    const jsonData = await get.json();
     
-    setData(jsonData.results)
+    setData(jsonData.results);
+  };
 
-  }
   return (
-     <>
-        <div className='container'>
+    <>
+      <div className='container'>
         <h1>Image Search App</h1>
-          <div className='inputs'>
-            <input type='text' placeholder='Search Images..' onChange={handleSearch}/>
-            <button onClick={getData} >Search</button>
-          </div>
-          <div className='images'>
+        <div className='inputs'>
+          <input type='text' placeholder='Search Images..' onChange={handleSearch} />
+          <button onClick={getData}>Search</button>
+        </div>
+        <div className='images'>
           {
-            data.map((curVal, index)=>{
-              return(
-                  <img src={curVal.urls.full}/>
-              )
+            data.map((curVal, index) => {
+              return (
+                <img key={index} src={curVal.urls.full} alt={curVal.alt_description || "Image"} />
+              );
             })
           }
-
-          </div>
         </div>
-     </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Imageapp;                                     
+export default Imageapp;
